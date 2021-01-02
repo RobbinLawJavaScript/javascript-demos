406d7da8c5219acd4ffa894e75ba558f11a4e197
robbinl <robbinl@telusplanet.net>
2021-01-02 15:01:50 -0700

    added w1 demos

---
+++ w1/bb_dom_access/js/main.js
@@ -0,0 +1,31 @@
+
+// document.getElementsByTagName(selector) – returns a list of matched elements
+let someElements = document.getElementsByTagName("p");
+console.log('ElementsByTagName: ' + someElements[0].innerHTML);
+someElements = document.getElementsByTagName("h1");
+console.log('ElementsByTagName: ' + someElements[0].innerHTML);
+
+// document.getElementById(selector) – returns a single matched element
+let someElement = document.getElementById("someId");
+console.log('ElementById: ' + someElement.innerHTML);
+
+// document.getElementsByClassName(selector) – returns a list of matched elements
+someElements = document.getElementsByClassName("someClass");
+console.log('ElementsByClassName: ' + someElements[0].innerHTML);
+
+// document.querySelector(selector) – returns the first matched element
+someElement = document.querySelector("p");
+console.log('querySelectorByTag: ' + someElement.innerHTML);
+someElement = document.querySelector("#someId1");
+console.log('querySelectorById: ' + someElement.innerHTML);
+someElement = document.querySelector(".someClass1");
+console.log('querySelectorByClass: ' + someElement.innerHTML);
+
+
+// document.querySelectorAll(selector) – returns a list of matched elements
+someElements = document.querySelectorAll("p");
+console.log('querySelectorAllByTag: ' + someElements[0].innerHTML);
+someElements = document.querySelectorAll("#someId1");
+console.log('querySelectorAllById: ' + someElements[0].innerHTML);
+someElements = document.querySelectorAll(".someClass1");
+console.log('querySelectorAllByClass: ' + someElements[0].innerHTML);
---
+++ w1/cc_var_vs_let/js/main.js
@@ -0,0 +1,28 @@
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var
+
+// Previous to ES6, var was the only way to 
+// declare a variable. Variables declared in 
+// the global scope are added to the global
+// object (window in the browser) and all vars
+// in a function are hoisted to the top of the 
+// function
+var x = 1;
+if (x === 1) {
+  var x = 2;
+  console.log(x);
+  // expected output: 2
+}
+console.log(x);
+// expected output: 2
+
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let
+let y = 1;
+if (y === 1) {
+  let y = 2;
+  console.log(y);
+  // expected output: 2
+}
+console.log(y);
+// expected output: 1
+
