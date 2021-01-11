1af4de85678db4562c44e49601f31603f126b5a7
robbinl <robbinl@telusplanet.net>
2021-01-11 09:39:03 -0700

    change

--- bb-functions/aa-functions-noparms/js/main.js
+++ bb-functions/aa-functions-noparms/js/main.js
@@ -1,12 +1,12 @@
 // basic function declaration with no parameters.
 function greetUser() {
-    let name;
+    let myname;
     const GREETING = 'Good afternoon ';
     console.log('before prompt');
-    name = prompt('Enter name:');
+    myname = prompt('Enter name:');
     console.log('after prompt');
     console.log('before alert');
-    alert(GREETING + name);
+    alert(GREETING + myname);
     console.log('after alert');
 }
 //calling the function with no arguments.
