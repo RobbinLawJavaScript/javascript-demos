abc2aaed21c1d7b4c1502017f0e3b877e7e73299
robbinl <robbinl@telusplanet.net>
2021-02-22 09:22:20 -0700

    change to ++

--- ff-loops/bb-nodelist-intro/js/main.js
+++ ff-loops/bb-nodelist-intro/js/main.js
@@ -1,11 +1,11 @@
-var output = document.querySelector('.output');
-var list = '<ul>';
-var count; // count the number of list items
-var items; // access to list items
+let output = document.querySelector('.output');
+let list = '<ul>';
+let count; // count the number of list items
+let items; // access to list items
 
 // add list items
 //count++ is equivalent count += 1 and equivalent to count = count + 1
-for (count = 0; count < 10; count = count + 1) {
+for (count = 0; count < 10; count++) {
 	list = list + '<li>' + (count + 1) + '</li>';
 }
 
