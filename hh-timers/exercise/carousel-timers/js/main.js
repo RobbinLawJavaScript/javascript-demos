9acda204c3d7f4da5aaf37ede9231113c4950933
robbinl <robbinl@telusplanet.net>
2021-03-14 10:23:00 -0600

    a

--- hh-timers/exercise/carousel-timers/js/main.js
+++ hh-timers/exercise/carousel-timers/js/main.js
@@ -2,14 +2,6 @@ let images = ['mountain1.jpg', 'mountain2.jpg', 'mountain3.jpg'];
 let currentImg = 0;
 document.querySelector('.carousel>img').src = 'images/' + images[0]; 
 
-let idx;
-let imageTracker = document.querySelector('.image-tracker');
-for (idx = 0; idx < images.length; idx += 1) {
-    imageTracker.innerHTML += '<span class="control" data-idx="' + idx + '">&bull;</span>';
-}
-
-imageTracker.querySelector('span').classList.add('active');
-
 document.querySelector('.carousel').addEventListener('click', function (evt){
     if (evt.target.classList.contains('control')) {
         if (evt.target.classList.contains('next')) {
@@ -22,34 +14,7 @@ document.querySelector('.carousel').addEventListener('click', function (evt){
             if (currentImg === -1) {
                 currentImg = images.length - 1;
             }
-        } else {
-            currentImg = parseInt(evt.target.dataset.idx, 10);
         }
         document.querySelector('.carousel>img').src = 'images/' + images[currentImg];
-
-        imageTracker.querySelector('span.active').classList.remove('active');
-        imageTracker.querySelectorAll('span')[currentImg].classList.add('active');
-    }
-});
-
-document.addEventListener('keydown', function (evt) {
-    var click;
-    console.log('hi');
-
-    switch (evt.key) {
-        case "ArrowLeft":
-            // left arrow
-            click = new MouseEvent('click', {
-                'bubbles': true
-            });
-            document.querySelector('.prev').dispatchEvent(click);
-            break;
-        case "ArrowRight":
-            // right arrow
-            click = new MouseEvent('click', {
-                'bubbles': true
-            });
-            document.querySelector('.next').dispatchEvent(click);
-            break;
     }
 });
--- hh-timers/exercise/carousel-timers/js/robbinsolution.js
+++ hh-timers/exercise/carousel-timers/js/robbinsolution.js
@@ -2,14 +2,6 @@ let images = ['mountain1.jpg', 'mountain2.jpg', 'mountain3.jpg'];
 let currentImg = 0;
 document.querySelector('.carousel>img').src = 'images/' + images[0]; 
 
-let idx;
-let imageTracker = document.querySelector('.image-tracker');
-for (idx = 0; idx < images.length; idx += 1) {
-    imageTracker.innerHTML += '<span class="control" data-idx="' + idx + '">&bull;</span>';
-}
-
-imageTracker.querySelector('span').classList.add('active');
-
 document.querySelector('.carousel').addEventListener('click', function (evt){
     if (evt.target.classList.contains('control')) {
         if (evt.target.classList.contains('next')) {
@@ -22,39 +14,10 @@ document.querySelector('.carousel').addEventListener('click', function (evt){
             if (currentImg === -1) {
                 currentImg = images.length - 1;
             }
-        } else {
-            currentImg = parseInt(evt.target.dataset.idx, 10);
         }
-        updateSlide(currentImg);
+        document.querySelector('.carousel>img').src = 'images/' + images[currentImg];
     }
 });
-//Here we add a keydown event listener for the whole document.
-document.addEventListener('keydown', function (evt) {
-    var click;
-
-    switch (evt.key) {
-        case "ArrowLeft":
-            // left arrow
-            click = new MouseEvent('click', {
-                'bubbles': true
-            });
-            document.querySelector('.prev').dispatchEvent(click);
-            break;
-        case "ArrowRight":
-            // right arrow
-            click = new MouseEvent('click', {
-                'bubbles': true
-            });
-            document.querySelector('.next').dispatchEvent(click);
-            break;
-    }
-});
-
-function updateSlide(index) {
-    document.querySelector('.carousel>img').src = 'images/' + images[index];
-    document.querySelector('.image-tracker .active').classList.remove('active');
-    document.querySelectorAll('[data-idx]')[index].classList.add('active');
-}
 
 let slideshowInterval;
 slideshowInterval = setInterval(function () {
@@ -62,5 +25,5 @@ slideshowInterval = setInterval(function () {
     if (currentImg == images.length) {
         currentImg = 0;
     }
-    updateSlide(currentImg)
+    document.querySelector('.carousel>img').src = 'images/' + images[currentImg];
 }, 3000);
