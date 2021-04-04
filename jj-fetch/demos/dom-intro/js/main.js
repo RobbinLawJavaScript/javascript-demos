adfd3194c25f3fadcf3fdd13fd45e98c871e54ae
robbinl <robbinl@telusplanet.net>
2021-04-04 13:21:59 -0600

    a

--- jj-fetch/demos/dom-intro/js/main.js
+++ jj-fetch/demos/dom-intro/js/main.js
@@ -1,21 +1,17 @@
+import * as myDemo1 from '/demos/demo1.js';
+import * as myDemo2 from '/demos/demo2.js';
+import * as myDemo3 from '/demos/demo3.js';
+import * as myDemo4 from '/demos/demo4.js';
+import * as myDemo5 from '/demos/demo5.js';
+import * as myDemo6 from '/demos/demo6.js';
+import * as myDemo7 from '/demos/demo7.js';
+import * as myDemo8 from '/demos/demo8.js';
+myDemo1.demo();
+//myDemo2.demo();
+//myDemo3.demo();
+//myDemo4.demo();
+//myDemo5.demo();
+//myDemo6.demo();
+//myDemo7.demo();
+//myDemo8.demo();
 
-
-
-const remoteDataURL = "your-unique-URL"
-const getRemoteBones = async () => {
-//Change the URL, Add Optional Arguments:
-const res = await fetch(remoteDataURL, {
-   //Optional arguments
-    method: 'GET',
-    headers: {
-       'Accept': 'application/json. text/plain, */*',
-       "Content-Type": "application/json",
-    },
-  }
-)
-const data = await res.json();
-return data;
-}
-let retrivedBones = getRemoteBones()
-.then(data => console.log('resolved', data))
-.catch((err) => console.log('rejected', err.message))
