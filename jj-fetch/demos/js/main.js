9d15afc911e9d5da76914016f2236c118c8674ea
robbinl <robbinl@telusplanet.net>
2021-04-04 13:46:10 -0600

    a

---
+++ jj-fetch/demos/js/main.js
@@ -0,0 +1,17 @@
+import * as myDemo1 from './demo1.js';
+import * as myDemo2 from './demo2.js';
+import * as myDemo3 from './demo3.js';
+import * as myDemo4 from './demo4.js';
+import * as myDemo5 from './demo5.js';
+import * as myDemo6 from './demo6.js';
+import * as myDemo7 from './demo7.js';
+import * as myDemo8 from './demo8.js';
+myDemo1.demo();
+//myDemo2.demo();
+//myDemo3.demo();
+//myDemo4.demo();
+//myDemo5.demo();
+//myDemo6.demo();
+//myDemo7.demo();
+//myDemo8.demo();
+
