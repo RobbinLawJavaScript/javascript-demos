b6edaf1a64f9d06fafa71619085a8402c40855aa
robbinl <robbinl@telusplanet.net>
2021-04-09 10:04:30 -0600

    a

--- jj-fetch/demos/js/main.js
+++ jj-fetch/demos/js/main.js
@@ -1,5 +1,11 @@
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises
 
+//Promises and Asyncronous javascript by Brad Traversy
+//https://www.youtube.com/watch?v=PoRJizFvM7s
+
+//Fetch API by Brad Traversy
+//https://www.youtube.com/watch?v=Oive66jrwBs
+
 //import * as myDemo from './demo1.js';
 //import * as myDemo from './demo2.js';
 //import * as myDemo from './demo3.js';
