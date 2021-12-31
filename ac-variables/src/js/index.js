561f9f023e2bc0af37c5572214f64a291699d21d
robbinl <robbinl@telusplanet.net>
2021-12-31 16:38:30 -0700

    a

--- ac-variables/src/js/index.js
+++ ac-variables/src/js/index.js
@@ -1,3 +1,12 @@
+// letters, numbers, _, $
+// Can not start with number
+
+// Multi word variables
+// var firstName = 'John'; // Camel case
+// var first_name = 'Sara'; // Underscore
+// var FirstName = 'Tom'; // Pascal case
+// var firstname;
+
 // var, let, const
 var myName;
 console.log(myName);
@@ -5,15 +14,6 @@ myName = 'John';
 console.log(myName);
 var myName2 = 'Jane';
 console.log(myName2);
-
-// // letters, numbers, _, $
-// // Can not start with number
-
-// // Multi word vars
-// var firstName = 'John'; // Camel case
-// var first_name = 'Sara'; // Underscore
-// var FirstName = 'Tom'; // Pascal case
-// var firstname;
 
 // LET
 let myName3;
@@ -28,19 +28,20 @@ const myName5 = 'James';
 console.log(myName5);
 // Can not reassign
 //myName5 = 'Sara';
-// Have to assign a value
+// Have to assign a value at declaration
 //const myName6;
 
+// CONST Literal Object
 const person = {
   name: 'John',
   age: 30
 }
 console.log(person);
-person.name = 'Sara';
-person.age = 32;
-console.log(person);
+// person.name = 'Sara';
+// person.age = 32;
+// console.log(person);
 
+// CONST Array
 const numbers = [1,2,3,4,5];
 numbers.push(6);
-
 console.log(numbers);
