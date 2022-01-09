d9fff24f9f7183a5316a7e422fec71059377bd3a
robbinl <robbinl@telusplanet.net>
2022-01-09 16:44:50 -0700

    a

--- ac-variables/src/js/variable-scope-2.js
+++ ac-variables/src/js/variable-scope-2.js
@@ -8,7 +8,7 @@ const c = 3;
 //   var a = 4;
 //   let b = 5;
 //   const c = 6;
-//   console.log('Function Block Scope: ', a, b, c);
+//   console.log('Function Scope: ', a, b, c);
 // }
 // test();
 
@@ -20,19 +20,20 @@ const c = 3;
 //   console.log('If Block Scope: ', a, b, c);
 // }
 
-// for(var a = 0; a < 10; a++) {
-//   console.log(`Loop Block Scope: ${a}`);
-// }
 
 // for(let a = 0; a < 10; a++) {
 //     console.log(`Loop Block Scope: ${a}`);
 // }
 
-// for(var b = 0; b < 10; b++) {
-//     console.log(`Loop Block Scope: ${b}`);
+// for(var a = 0; a < 10; a++) {
+//   console.log(`Loop Block Scope: ${a}`);
 // }
 
 // for(let b = 0; b < 10; b++) {
+//     console.log(`Loop Block Scope: ${b}`);
+// }
+
+// for(var b = 0; b < 10; b++) {
 //     console.log(`Loop Block Scope: ${b}`);
 // }
 
