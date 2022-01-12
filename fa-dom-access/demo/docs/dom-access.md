2d7dd9a5db4d5b67908dd4e7052db4cbbd54aadd
robbinl <robbinl@telusplanet.net>
2022-01-12 06:51:42 -0700

    a

---
+++ fa-dom-access/demo/docs/dom-access.md
@@ -0,0 +1,11 @@
+# DOM Access
+
+**Resources** :
+
+- [getElementById](https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById)
+- [getElementsByTagName](https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementsByTagName)
+- [getElementsByClassName](https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementsByClassName)
+- [querySelector](https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector)
+- [querySelectorAll](https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll)
+
+----
--- fa-dom-access/demo/src/js/dom-access.js
+++ fa-dom-access/demo/src/js/dom-access.js
@@ -2,39 +2,56 @@ export function demo() {
 
 //DOM Access techniques
 
-// document.getElementsByTagName(selector) – returns a list of matched elements
-let someElements = document.getElementsByTagName("p");
-console.log('ElementsByTagName: ' + someElements[0].innerHTML);
-console.log('ElementsByTagName: ' + someElements[1].innerHTML);
-console.log('ElementsByTagName: ' + someElements[2].innerHTML);
-someElements = document.getElementsByTagName("h1");
-console.log('ElementsByTagName: ' + someElements[0].innerHTML);
+// document.getElementById(selector) – returns the first matched element
+// There is NO getElementsById because each Id should be unique
 
-// document.getElementById(selector) – returns a single matched element
-let someElement = document.getElementById("someId");
-console.log('ElementById: ' + someElement.innerHTML);
+// document.getElementsByTagName(selector) – returns a collection of matched elements
 
-// document.getElementsByClassName(selector) – returns a list of matched elements
-someElements = document.getElementsByClassName("someClass");
-console.log('ElementsByClassName: ' + someElements[0].innerHTML);
-console.log('ElementsByClassName: ' + someElements[1].innerHTML);
+// document.getElementsByClassName(selector) – returns a collection of matched elements
 
 // document.querySelector(selector) – returns the first matched element
-someElement = document.querySelector("p");
-console.log('querySelectorByTag: ' + someElement.innerHTML);
-someElement = document.querySelector("#someId1");
-console.log('querySelectorById: ' + someElement.innerHTML);
-someElement = document.querySelector(".someClass1");
-console.log('querySelectorByClass: ' + someElement.innerHTML);
 
+// document.querySelectorAll(selector) – returns a NodeList of matched elements
 
-// document.querySelectorAll(selector) – returns a list of matched elements
-someElements = document.querySelectorAll("p");
-console.log('querySelectorAllByTag: ' + someElements[0].innerHTML);
-someElements = document.querySelectorAll("#someId1");
-console.log('querySelectorAllById: ' + someElements[0].innerHTML);
-console.log('querySelectorAllById: ' + someElements[1].innerHTML);
-someElements = document.querySelectorAll(".someClass1");
-console.log('querySelectorAllByClass: ' + someElements[0].innerHTML);
+let ref;
+let val;
 
+// ref = document.getElementById("someId");
+// val = ref;
+// val = ref.innerHTML;
+
+// ref = document.getElementsByTagName("p");
+// val = ref;
+// val = ref[0];
+// val = ref[0].innerHTML;
+// val = ref[1];
+// val = ref[1].innerHTML;
+
+// ref = document.getElementsByClassName("someClass");
+// val = ref;
+// val = ref[0];
+// val = ref[0].innerHTML;
+// val = ref[1];
+// val = ref[1].innerHTML;
+
+// ref = document.querySelector("p");
+// val = ref.innerHTML;
+
+// ref = document.querySelector("#someId1");
+// val = ref.innerHTML;
+
+// ref = document.querySelector(".someClass");
+// val = ref.innerHTML;
+
+// ref = document.querySelectorAll("p");
+// val = ref[0].innerHTML;
+
+// ref = document.querySelectorAll("#someId1");
+// val = ref[0].innerHTML;
+
+// ref = document.querySelectorAll(".someClass1");
+// val = ref[0].innerHTML;
+// val = ref[1].innerHTML;
+
+console.log(val);
 }
