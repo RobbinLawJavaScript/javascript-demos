dbeef2002b8fb15e8d739dfbb4abd2a0a8e32e00
robbinl <robbinl@telusplanet.net>
2022-01-14 11:17:53 -0700

    a

--- fa-dom-access/demo/src/js/dom-access.js
+++ fa-dom-access/demo/src/js/dom-access.js
@@ -16,44 +16,46 @@ export function demo() {
 let ref;
 let val;
 
-// ref = document.getElementById("someId");
-// val = ref;
-// val = ref.innerHTML;
+ref = document.getElementById("someId");
+val = ref;
+val = ref.innerHTML;
+//val = ref.innerHTML.innerHTML;
+val = ref.innerText;
 
-// ref = document.getElementsByTagName("p");
-// val = ref;
-// val = ref[0];
-// val = ref[0].innerHTML;
-// val = ref[1];
-// val = ref[1].innerHTML;
+ref = document.getElementsByTagName("p");
+val = ref;
+val = ref[0];
+val = ref[0].innerHTML;
+val = ref[1];
+val = ref[1].innerHTML;
 
-// ref = document.getElementsByClassName("someClass");
-// val = ref;
-// val = ref[0];
-// val = ref[0].innerHTML;
-// val = ref[1];
-// val = ref[1].innerHTML;
+ref = document.getElementsByClassName("someClass");
+val = ref;
+val = ref[0];
+val = ref[0].innerHTML;
+val = ref[1];
+val = ref[1].innerHTML;
 
-// ref = document.querySelector("p");
-// val = ref.innerHTML;
+ref = document.querySelector("p");
+val = ref.innerHTML;
 
-// ref = document.querySelector("#someId1");
-// val = ref.innerHTML;
+ref = document.querySelector("#someId1");
+val = ref.innerHTML;
 
-// ref = document.querySelector(".someClass");
-// val = ref.innerHTML;
+ref = document.querySelector(".someClass");
+val = ref.innerHTML;
 
-// ref = document.querySelectorAll("p");
-// val = ref;
-// val = ref[0].innerHTML;
-// val = ref[0].innerText;
+ref = document.querySelectorAll("p");
+val = ref;
+val = ref[0].innerHTML;
+val = ref[0].innerText;
 
-// // ref = document.querySelectorAll("#someId1");
-// // val = ref[0].innerHTML;
+ref = document.querySelectorAll("#someId1");
+val = ref[0].innerHTML;
 
-// ref = document.querySelectorAll(".someClass1");
-// val = ref;
-// val = ref[0].innerHTML;
+ref = document.querySelectorAll(".someClass1");
+val = ref;
+val = ref[0].innerHTML;
 //val = ref[1].innerHTML;
 
 console.log(val);
