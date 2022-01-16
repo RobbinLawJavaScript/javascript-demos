a1b2990e954022657142c69930dbc266e5de6856
robbinl <robbinl@telusplanet.net>
2022-01-16 09:35:17 -0700

    a

--- ea-events/demos/src/js/index.js
+++ ea-events/demos/src/js/index.js
@@ -1,6 +1,3 @@
 import * as myDemo from './main.js';
-//import * as myDemo from './bb-function-parms.js';
-//import * as myDemo from './cc-function-return-val.js';
-//import * as myDemo from './dd-scope.js';
 
 myDemo.demo();
