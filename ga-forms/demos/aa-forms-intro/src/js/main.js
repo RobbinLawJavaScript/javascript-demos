8629dfa712ca51b681538aa26dde7366d2a44252
robbinl <robbinl@telusplanet.net>
2022-01-20 16:55:48 -0700

    a

--- ga-forms/demos/aa-forms-intro/src/js/main.js
+++ ga-forms/demos/aa-forms-intro/src/js/main.js
@@ -1,30 +1,29 @@
 export function demo() {
 
-const formRef = document.querySelector('.frm');
+const formRef = document.querySelector('form.frm');
 formRef.addEventListener('submit', FormSubmitHandler);
 function FormSubmitHandler(evt) {
 
-// the target of the submit event is the 'form'
+// The target of the submit event is the 'form'
 // element itself (you can submit the form by
 // either pressing <enter> or clicking the submit
 // button).
 // e.g. evt.target will give access to the form
 
-let output = document.querySelector('div.output');
+const successMessage = document.querySelector('div.success-message');
+const errorMessage = document.querySelector('div.error-message');
 
-// access the input inputname
-// the elements property of a form element gives access
+// The elements property of a form element gives access
 // to the form control elements in the form (e.g. input and button)
 // You can use either .validName access to the name (if it's a valid name)
 // or ['invalid-name'] for names that do not 
 // adhere to JavaScript naming conventions. In JavaScript an invalid name
-//would have a - in it.
+// would have a - in it. Don't use a - in any name but css classes.
 
-// display the value in the output
-output.innerHTML = evt.target.elements.inputname.value;
-//output.innerHTML = evt.target.elements.inputid.value;
-//output.innerHTML = evt.target.elements.silly.value;
-//output.innerHTML = evt.target.elements['input-name'].value;
+successMessage.innerHTML = evt.target.elements.firstName.value;
+//successMessage.innerHTML = evt.target.elements.firstNameId.value;
+//successMessage.innerHTML = evt.target.elements.silly.value;
+//successMessage.innerHTML = evt.target.elements['input-name'].value;
 
 evt.preventDefault(); 
 }
