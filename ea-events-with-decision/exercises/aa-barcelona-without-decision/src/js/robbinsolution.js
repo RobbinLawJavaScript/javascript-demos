70fc43cdc85068bde38bdb74252e93eed8addc9a
robbinl <robbinl@telusplanet.net>
2022-01-28 11:17:46 -0700

    a

--- ea-events-with-decision/exercises/aa-barcelona-without-decision/src/js/robbinsolution.js
+++ ea-events-with-decision/exercises/aa-barcelona-without-decision/src/js/robbinsolution.js
@@ -4,23 +4,21 @@ export function demo() {
 
 //reference the p.city-intro>a element.
 const featureLink = document.querySelector('p.city-intro>a');
-
+featureLink.addEventListener('click', featureLinkHandler);
 //Use the debugger in Chrome to see what is happening at each statement.
 function featureLinkHandler(e) {
     //preventDefault stops the normal operation of the href in the target element (p.city-intro>a element).
     e.preventDefault();
     //get a reference to the img.feature element.    
-    let featureImage = document.querySelector('img.feature');
+    const featureImage = document.querySelector('img.feature');
     //change the src of the img.feature element to be the href of the target element (p.city-intro>a element).     
     featureImage.src = e.target.href;
     //remove the hidden class from the img.feature element.     
     featureImage.classList.remove('hidden');
 }
 
-//featureLink.addEventListener('click', featureLinkHandler);
-
 //There are many different events to listen to.
 //featureLink.addEventListener('mouseover', featureLinkHandler);
-featureLink.addEventListener('mouseout', featureLinkHandler);
+//featureLink.addEventListener('mouseout', featureLinkHandler);
 
 }
