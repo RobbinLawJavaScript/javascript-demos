874408e5ad2758e880efdd950e020125d040661e
robbinl <robbinl@telusplanet.net>
2022-02-01 10:05:43 -0700

    a

--- ga-forms/exercise/src/js/begin.js
+++ ga-forms/exercise/src/js/begin.js
@@ -1,3 +1,63 @@
 export function Run(){
-  
-  }
+
+	const form = document.querySelector("#form")
+	
+	form.addEventListener("submit", e => {
+		e.preventDefault();    
+		let text = getUserValues(e);
+		//TODO: Add code here to trim the entered text.
+
+		let {message, isNotValid} = validateUserValues(text);
+		if(isNotValid){
+			showAlert(message, 'error');
+		} else {
+			AddItemToList(text);
+			showAlert('Tag Added to List!', 'success');
+			clearUserValues(e);
+		}
+	});
+	
+	function getUserValues(e){
+		let text = e.target.elements["textName"].value;
+		return text;
+	}
+	
+	function validateUserValues(text){
+		let message = '';
+		let isNotValid = true;
+		//TODO: Add code here to validate the entered text
+		//as per the specs request.
+		
+		if(message != '')
+			message = `Invalid Data: ` + message;
+		else isNotValid = false;
+		return {message, isNotValid};
+	}
+	
+	function showAlert(message, className) {
+		if(message != ''){
+			const parentDiv = document.querySelector('#alert');
+			const alertElement = 
+			`
+			<div class='${className} form-control'>
+				${message}
+			</div>
+			`
+			parentDiv.innerHTML = alertElement;
+		}
+	}
+	
+	function AddItemToList(text) {
+		let list = document.querySelector("#list")
+		let newItem = 
+		`
+			<p>#${text}</p>
+		`
+		list.innerHTML =  newItem + list.innerHTML;
+	}
+	
+	function clearUserValues(e) {
+		e.target.elements["textName"].value = '';
+	}
+	
+	}
