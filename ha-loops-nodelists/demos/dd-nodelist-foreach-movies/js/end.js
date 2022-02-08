6f3c83241a0ef4ae5b7661a73a3bd16bab9ae98b
robbinl <robbinl@telusplanet.net>
2022-02-08 16:33:41 -0700

    a

--- ha-loops-nodelists/demos/dd-nodelist-foreach-movies/js/end.js
+++ ha-loops-nodelists/demos/dd-nodelist-foreach-movies/js/end.js
@@ -1,25 +1,20 @@
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
+
 function End(){
 
 let movieFilterForm = document.querySelector("#movie-filter-form")
 movieFilterForm.elements["filter-query"].focus()
 
 let allMovieItems = document.querySelectorAll(".top-movies-list li")
+console.log(allMovieItems);
 
 movieFilterForm.addEventListener("submit", evt => {
-	evt.preventDefault()
-	let filter = movieFilterForm.elements["filter-query"].value
-	console.log(movieFilterForm.elements["filter-query"].value)
-	filterItems(filter)
+	evt.preventDefault();
+	let filter = movieFilterForm.elements["filter-query"].value;
+	console.log(filter);
+	filterItems(filter);
 });
 
-/*
-We're going to check each item contains our search query
-- if it's not then we're going to add the "hidden-item" class
-- if does include it we're going to remove the "hidden-item" class.
-
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
-
-*/
 const filterItems = filterValue => {
 	allMovieItems.forEach(movieItem => {
 		if (movieItem.innerText.toLowerCase().includes(filterValue.toLowerCase())) {
