a7149baca8af525a12d1e245ff0226aa3a3e83e9
robbinl <robbinl@telusplanet.net>
2022-02-15 10:58:56 -0700

    a

--- bc-data-types/src/js/arrays.js
+++ bc-data-types/src/js/arrays.js
@@ -3,33 +3,35 @@ export function demo() {
 // Create some arrays
 const numbers = [43,56,33,23,44,36,5];
 const numbers2 = new Array(22,45,33,76,54);
-const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
+const fruit = ['Banana', 'Pear', 'Orange', 'Apple'];
 const mixed = [22, 'Hello', true, undefined, null, {a:1, b:1}, new Date()];
 
 let val;
 
+
 val = numbers.length;
-//val = Array.isArray(numbers);
-//val = numbers[3];
-//val = numbers[0];
-//numbers[2] = 100;
-//val = numbers.indexOf(36);
+// val = Array.isArray(numbers);
+// val = numbers[3];
+// val = numbers[0];
+// numbers[2] = 100;
+// val = numbers.indexOf(36);
+// val = numbers.indexOf(40);
 
 // numbers.push(250);
 // numbers.unshift(120);
 // numbers.pop();
 // numbers.shift();
-// numbers.splice(1,3);
-// numbers.reverse();
+//numbers.splice(1,3);
+//numbers.reverse();
 
 //val = numbers.concat(numbers2);
-
+//numbers.sort();
 //val = fruit.sort();
-// val = numbers.sort();
+//val = numbers.sort();
 
 // // Use the "compare function"
 // val = numbers.sort(function(x, y){
-//   return x - y;
+//    return x - y;
 // });
 
 // // Reverse sort
@@ -42,7 +44,8 @@ function over50(num){
   return num > 50;
 }
 
-//val = numbers.find(over50);
+// val = numbers.find(over50);
+// val = mixed;
 
 console.log(numbers);
 console.log(val);
