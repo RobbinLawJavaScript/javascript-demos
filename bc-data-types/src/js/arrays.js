419ea75f6649132826b7bdbcfb23fb296bcaba31
robbinl <robbinl@telusplanet.net>
2022-02-15 10:09:04 -0700

    a

--- bc-data-types/src/js/arrays.js
+++ bc-data-types/src/js/arrays.js
@@ -8,37 +8,23 @@ const mixed = [22, 'Hello', true, undefined, null, {a:1, b:1}, new Date()];
 
 let val;
 
-// Get array length
 val = numbers.length;
-// Check if is array
-val = Array.isArray(numbers);
-// Get single value
-val = numbers[3];
-val = numbers[0];
-// Insert into array
-numbers[2] = 100;
-// Find index of value
-val = numbers.indexOf(36);
+//val = Array.isArray(numbers);
+//val = numbers[3];
+//val = numbers[0];
+//numbers[2] = 100;
+//val = numbers.indexOf(36);
 
-// MUTATING ARRAYS
-// // Add on to end
 // numbers.push(250);
-// // Add on to front
 // numbers.unshift(120);
-// // Take off from end
 // numbers.pop();
-// // Take off from front
 // numbers.shift();
-// // Splice values
 // numbers.splice(1,3);
-// // Reverse
 // numbers.reverse();
 
-// Concatenate array
-val = numbers.concat(numbers2);
+//val = numbers.concat(numbers2);
 
-// Sorting arrays
-val = fruit.sort();
+//val = fruit.sort();
 // val = numbers.sort();
 
 // // Use the "compare function"
@@ -56,7 +42,7 @@ function over50(num){
   return num > 50;
 }
 
-val = numbers.find(over50);
+//val = numbers.find(over50);
 
 console.log(numbers);
 console.log(val);
