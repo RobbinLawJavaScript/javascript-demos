48393d0bd9e4d40b00eb629b8543d09c270d60fe
robbinl <robbinl@telusplanet.net>
2022-03-05 13:23:41 -0700

    a

--- da-functions/demos/src/js/bb-function-parms.js
+++ da-functions/demos/src/js/bb-function-parms.js
@@ -10,11 +10,9 @@ greetUser1(myName);
 
 //Basic function declaration with a parameter.
 function greetUser1(name) {
-    document.querySelector('p.greeting1')
-        .innerHTML = `Good morning ${name}`;
+	document.querySelector('p.greeting1')
+	.innerHTML = `Good morning ${name}`;
 }
-
-//greetUser2(myName);
 
 // Functions can be defined in several ways, each with different
 // implications.
@@ -23,23 +21,25 @@ function greetUser1(name) {
 // assigning it to a variable. Also demonstrates use of an anonymous
 // function (i.e. a function lacking an explicit name).
 // The name of the function will be the variable name.
-let greetUser2 = function(name) {
-    document.querySelector('p.greeting2')
-        .innerHTML = `Good afternoon ${name}`;
+// With Function Expressions we cannot call the function before it is declared.
+//greetUser2(myName);
+const greetUser2 = function(name) {
+	document.querySelector('p.greeting2')
+	.innerHTML = `Good afternoon ${name}`;
 }
-//Here with Function Expressions we cannot call the function before it is declared.
+// With Function Expressions we cannot call the function before it is declared.
 greetUser2(myName);
+
 
 // Arrow functions are an ES6 update to the language that allows for a more
 // elegant syntax of defining function expressions. The function reserved
 // word is dropped in favour of the 'arrow' (=>). Arrow functions also have
 // a syntax that implicitly returns a value, something that will be explored
 // later on. Otherwise, the function can be used as shown previously. 
-let greetUser3 = (name) => {
-    document.querySelector('p.greeting3')
-        .innerHTML = `Good night ${name}`;
+const greetUser3 = (name) => {
+	document.querySelector('p.greeting3')
+	.innerHTML = `Good night ${name}`;
 }
-//Here with Function Expressions we cannot call the function before it is declared.
+// With Function Expressions we cannot call the function before it is declared.
 greetUser3(myName);
-
 }
