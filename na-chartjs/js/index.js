9427c5ede291f75058aae40bd60aaeef2f623f4c
robbinl <robbinl@telusplanet.net>
2022-04-09 17:32:53 -0600

    a

--- na-chartjs/js/index.js
+++ na-chartjs/js/index.js
@@ -5,11 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import our custom css
 import '../css/main.css';
 
-// that's right you'll see that you'll be using 
-// a lot more js to load your css as well.
-
-
-
 import {renderFoodThoughtsChart} from './food-thoughts-chart.js';
 import {renderFoodTimesChart} from './food-times-chart.js';
 
