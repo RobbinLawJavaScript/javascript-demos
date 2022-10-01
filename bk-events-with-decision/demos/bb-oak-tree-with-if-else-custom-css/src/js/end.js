b6c805aabcc6813e6b7b27c293d3e1624d2d588f
robbinl <robbinl@telusplanet.net>
2022-10-01 13:34:44 -0600

    events demos

--- bk-events-with-decision/demos/bb-oak-tree-with-if-else-custom-css/src/js/end.js
+++ bk-events-with-decision/demos/bb-oak-tree-with-if-else-custom-css/src/js/end.js
@@ -1,14 +1,9 @@
 export function demo() {
-//TODO:
-//With this code the hidden class is always removed, but
-//what if we want to remove the hidden class IF it is present,
-//otherwise, add it back!
 
 const featureImage = document.querySelector('img.feature');
 featureImage.addEventListener('click', handleFeatureClick);
 function handleFeatureClick() {
 	const desc = document.querySelector('p.feature.description');
-	//TODO: Change the code here
 	if (desc.classList.contains('hidden')) 
 		desc.classList.remove('hidden');
 	 else 
