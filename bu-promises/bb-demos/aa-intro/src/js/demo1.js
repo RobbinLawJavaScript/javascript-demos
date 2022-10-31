989090848424dd58216d273b019422c3e84b8cb3
robbinl <robbinl@telusplanet.net>
2022-10-31 11:12:24 -0600

    a

--- bu-promises/bb-demos/aa-intro/src/js/demo1.js
+++ bu-promises/bb-demos/aa-intro/src/js/demo1.js
@@ -1,8 +1,10 @@
 function Demo1(){
 
 function getPromise() {
+  console.log('create and return a promise state is pending');
   return new Promise((resolve, reject) => {
     setTimeout(() => {
+      console.log('timer has timed out');
       resolve(['Success:', 'We got an array of strings', 'hi there']);
       reject('Error: Something went wrong');
       resolve({Name: 'Robbin', Age: 62});
