4ccc5c56e9aa385c8cae7af0297faaf4982a96ea
robbinl <robbinl@telusplanet.net>
2022-11-02 17:51:20 -0600

    a

--- bs-loops-arrays-of-objects/demos/js/object-literals.js
+++ bs-loops-arrays-of-objects/demos/js/object-literals.js
@@ -1,32 +1,67 @@
 export function demo() {
 
-const person = {
-  firstName: 'Steve',
-  lastName: 'Smith',
-  age: 36,
-  email: 'steve@aol.com',
-  hobbies: ['music', 'sports'],
-  address: {
-    city: 'Miami',
-    state: 'FL'
+  const myArrayOfStrings =
+  [
+    'Hey man',
+    'how are you',
+    'this is fun',
+  ];
+
+  const myArrayOfObjects =
+  [
+    {
+        name: "Steve Something",
+        email: "steve.something@fake.test.com"
+    },
+    {
+        name: "gary else",
+        email: "gary.else@fake.test.com"
+    },
+    {
+        name: "elsa ellie",
+        email: "elsa.ellie@fake.test.com"
+    },
+  ];
+
+
+  const renderStringData = (arrayOfStrings) => {
+    let stringList = document.querySelector("#strings");
+    stringList.innerHTML = "";
+    console.log('arrayOfStrings type of each data element: ', typeof(arrayOfStrings));
+    // obj is a datatype "object" which we cannot use with .foreach()
+    // here we must use .map()
+    arrayOfStrings.foreach(str => {
+      stringList.innerHTML += 
+      `
+      <div class="list-group-item list-group-item-action">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">${str}</h5>
+        </div>
+        <small>${str}</small>
+      </div>
+      `
+    });
   }
-}
 
-let val1 = person;
-// Get specific value
-let val2 = person.firstName;
-let val3 = person['lastName'];
-let val4 = person.age;
-let val5 = person.hobbies[1];
-let val6 = person.address.state;
-let val7 = person.address['city'];
+  const renderObjectData = (arrayOfObjects) => {
+    let objectsList = document.querySelector("#objects");
+    objectsList.innerHTML = "";
+    console.log('arrayOfObjects type of each data element: ', typeof(arrayOfObjects));
+    // obj is a datatype "object" which we cannot use with .foreach()
+    // here we must use .map()
+    arrayOfObjects.map(obj => {
+      objectsList.innerHTML += 
+      `
+      <div class="list-group-item list-group-item-action">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">${obj.name}</h5>
+        </div>
+        <small>${obj.email}</small>
+      </div>
+      `
+    });
+  }
 
-console.log(val1);
-console.log(val2);
-console.log(val3);
-console.log(val4);
-console.log(val5);
-console.log(val6);
-console.log(val7);
-
+  renderStringData(myArrayOfStrings);
+  renderObjectData(myArrayOfObjects);
 }
