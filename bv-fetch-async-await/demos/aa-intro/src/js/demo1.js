d8da230cdf88a0169abfc129144b098e509ff37f
robbinl <robbinl@telusplanet.net>
2022-11-03 14:47:26 -0600

    a

--- bv-fetch-async-await/demos/aa-intro/src/js/demo1.js
+++ bv-fetch-async-await/demos/aa-intro/src/js/demo1.js
@@ -1,25 +1,25 @@
 function Demo1() {
 	
-	//const localDataURL = './data/bones.json';
+	const localDataURL = './data/bones.json';
 	//const localDataURL = './data/bones2.json';
-	const localDataURL = './data/bad-bones.json';
+	//const localDataURL = './data/bad-bones.json';
 
-	console.log('begin of program');
+	console.log(1);
 	fetch(localDataURL)
 	.then((res) => {
-		console.log('then response: ', res);
+		console.log('fetch.then response: ', res);
 		if (!res.ok) {
 			throw new Error('OOPS');
-	 	}
+		}
 		return res.json();
 	})
 	.then(data => {
-		console.log('then data: ', data);
+		console.log('fetch.then data: ', data);
 	})
 	.catch((err) => {
-		console.log('catch error: ', err);
+		console.log('fetch.catch error: ', err);
 	});
-	console.log('promise from fetch returned to caller with state pending');
+	console.log(2);
 }
 
 export default Demo1;
--- bv-fetch-async-await/demos/aa-intro/src/js/demo2.js
+++ bv-fetch-async-await/demos/aa-intro/src/js/demo2.js
@@ -1,39 +1,33 @@
 function Demo2() {
 
-	const localDataURL = './data/bones.json';
+	//const localDataURL = './data/bones.json';
 	//const localDataURL = './data/bones2.json';
-	//const localDataURL = './data/bad-bones.json';
+	const localDataURL = './data/bad-bones.json';
 
-	const getPromise = () => {
+	// async and await are part of the ES7 spec
+	// async functions always returns a promise
+	const getData = async () => {
 		console.log(2);
-		return new Promise((resolve, reject) => {
-			console.log(3);
-			fetch(localDataURL)
-			.then((res) => {
-				console.log('first then in getPromise response: ', res);
-				if (!res.ok) {
-					throw new Error('OOPS BAD RESPONSE');
-				}
-				return res.json();
-			})
-			.then((data) => {
-				console.log('second then in getPromise data: ', data);
-				resolve(data);
-			})
-			.catch((err) => {
-				console.log('catch in getPromise error: ', err);
-				reject(err);
-			});
-			console.log(4);
-		});
+		// fetch() returns a promise to res.
+		// The await keyword "stalls" the JS assignment
+		// until the data or error is returned (promise resolves or is rejected). 
+		const res = await fetch(localDataURL);
+		console.log('resolved response:', res);
+		if (!res.ok) {
+			throw new Error('OPPS');
+	 	}
+		// res.json() also returns a promise to data.
+		const data = await res.json();
+		console.log('resolved data:', data);
+		return data;
 	}
 
 	console.log(1);
-	getPromise()
-	.then(data => console.log('then data:', data))
-	.catch((err) => console.log('catch error:', err))
-	console.log(5);
-}
+	getData()
+	.then(data => console.log('getData.then data:', data))
+	.catch(err => console.log('getData.catch error:', err));
+	console.log(3);
 
+}
 
 export default Demo2;
