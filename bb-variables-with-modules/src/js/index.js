2f3572ae3604933cbb71f84958652ed681d22e1d
robbinl <robbinl@telusplanet.net>
2022-12-07 14:20:41 -0700

    a

--- bb-variables-with-modules/src/js/index.js
+++ bb-variables-with-modules/src/js/index.js
@@ -1,4 +1,5 @@
 import * as myDemo from './variable-basics.js';
+
 //Both of these advanced scope topics NOT for CPSC1520
 //import * as myDemo from './variable-advanced-scope-1.js';
 //import * as myDemo from './variable-advanced-scope-2.js';
--- bc-data-types/src/js/index.js
+++ bc-data-types/src/js/index.js
@@ -1,9 +1,11 @@
 import * as myDemo from './data-types.js';
-//import * as myDemo from './data-type-conversion.js';
 //import * as myDemo from './numbers.js';
 //import * as myDemo from './strings.js';
 //import * as myDemo from './arrays.js';
 //import * as myDemo from './object-literals.js';
+
+//Both of these next topics NOT for CPSC1520
+//import * as myDemo from './data-type-conversion.js';
 //import * as myDemo from './date-time.js';
 
 myDemo.demo();
