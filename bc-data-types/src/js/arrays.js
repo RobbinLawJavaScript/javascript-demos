06b46a75052896d0c724dae43ce02e7ed291659c
robbinl <robbinl@telusplanet.net>
2023-01-13 14:47:11 -0700

    a

--- bc-data-types/src/js/arrays.js
+++ bc-data-types/src/js/arrays.js
@@ -5,33 +5,33 @@ const numbers = [43,56,33,23,44,36,5];
 const numbers2 = new Array(22,45,33,76,54);
 const fruit = ['Banana', 'Pear', 'Orange', 'Apple'];
 const mixed = [22, 'Hello', true, undefined, null, {a:1, b:1}, new Date()];
-
+console.log(numbers);
 let val;
 
 
 val = numbers.length;
-//val = Array.isArray(numbers);
-//val = numbers[3];
-//val = numbers[0];
+val = Array.isArray(numbers);
+val = numbers[3];
+val = numbers[0];
 //numbers[2] = 100;
-//val = numbers.indexOf(36);
-//val = numbers.indexOf(40);
+val = numbers.indexOf(36);
+val = numbers.indexOf(40);
 
-//numbers.push(250);
-//numbers.unshift(120);
-//numbers.pop();
-//numbers.shift();
+numbers.push(250);
+numbers.unshift(120);
+numbers.pop();
+numbers.shift();
 //numbers.splice(1,3);
 //numbers.reverse();
 
-//val = numbers.concat(numbers2);
-//val = fruit.sort();
-//val = numbers.sort(); //This does NOT work on numbers
+val = numbers.concat(numbers2);
+val = fruit.sort();
+//val = numbers.sort(); //This does NOT work on numbers as it changes the numbers to strings and then sorts.
 
 // // Use the "compare function"
-// val = numbers.sort(function(x, y){
-//    return x - y;
-// });
+val = numbers.sort(function(x, y){
+   return x - y;
+});
 
 // // Reverse sort
 // val = numbers.sort(function(x, y){
@@ -40,6 +40,5 @@ val = numbers.length;
 
 //val = mixed;
 
-console.log(numbers);
 console.log(val);
 }
--- bc-data-types/src/js/index.js
+++ bc-data-types/src/js/index.js
@@ -1,7 +1,7 @@
-import * as myDemo from './data-types.js';
+//import * as myDemo from './data-types.js';
 //import * as myDemo from './numbers.js';
 //import * as myDemo from './strings.js';
-//import * as myDemo from './arrays.js';
+import * as myDemo from './arrays.js';
 //import * as myDemo from './object-literals.js';
 
 //Both of these next topics NOT for CPSC1520
