3fe050bd1d90309ffe65a970f3a91d78370596e0
robbinl <robbinl@telusplanet.net>
2023-03-16 09:43:14 -0600

    arrays

--- bq-loops-arrays/demos/dd-delete-item-from-a-list-FOODS/js/main.js
+++ bq-loops-arrays/demos/dd-delete-item-from-a-list-FOODS/js/main.js
@@ -9,35 +9,39 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild
 
 export default function Demo(){
-  let listOne = document.querySelector("#list-one");
+  let listOne = document.querySelector("#list-one")
   // listOneItems will be a NODE-LIST but acts like an array.
-  let listOneItems = document.querySelectorAll("#list-one li");
-  let myArray = [];
+  let listOneItems = document.querySelectorAll("#list-one li")
+  let myArray = []
 
   listOneItems.forEach((element) => {
     // Add the element to the end of the array.
-    myArray.push(element);
-  });
+    myArray.push(element)
+    console.log(`myArray elements: ${myArray}`)
+  })
 
   listOne.addEventListener("click", (event) => {
     let listOneItem = event.target
-    removeFromList(listOneItem);
+    removeFromList(listOneItem)
   })
   
   const removeFromList = (listOneItem) => {
     // Find the first occurrence of the 
-    // item (in this case a html li element)
+    // item (in this case a html li elements innerText)
     // in the array and return its index location in the array.
-    let index = myArray.indexOf(listOneItem);
-    console.log(index);
+    let index = myArray.findIndex((item) => item.innerText === listOneItem.innerText)
+    //let index = myArray.indexOf(listOneItem);
+    console.log(index)
     // Remove 1 element from the array at index location called "index".
-    myArray.splice(index, 1);
-    renderList();
+    myArray.splice(index, 1)
+    renderList()
   }
   
   const renderList = () => {
     listOne.replaceChildren()
     myArray.forEach((element)=> {
+      console.log(`typeof element:`, typeof(element))
+      console.log(`html li element contents:, ${element.innerText}`)
       listOne.appendChild(element)
     });
   }
