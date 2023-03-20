fd18c8b3a0f19e225b435b64fe0337c8974930a5
robbinl <robbinl@telusplanet.net>
2023-03-20 09:26:16 -0600

    fav foods with only arrays

--- bq-loops-arrays/demos/aa-FAVORITE-FOODS-with-only-Arrays/js/demo5.js
+++ bq-loops-arrays/demos/aa-FAVORITE-FOODS-with-only-Arrays/js/demo5.js
@@ -3,8 +3,6 @@ export default function Demo5(data){
   let form = document.querySelector("#form")
   form.elements["form-text"].focus();
   let listOne = document.querySelector("#list-one")
-  // listOneItems will be a NODE-LIST of li elements but acts like an array.
-  let listOneItems = document.querySelectorAll("#list-one li")
   let listTwo = document.querySelector("#list-two")
   let myArray = []
 
@@ -36,27 +34,30 @@ export default function Demo5(data){
 
   listOne.addEventListener("click", (event) => {
     const item = event.target.innerText
-    addItemToArrayAndRenderList(item, listTwo)    
+    addItemToArrayAndRenderList(item, listTwo, myArray)    
   })
 
-  const addItemToArrayAndRenderList = (item, list) => {
-    myArray.push(item)
-    renderList(list, myArray)
+  const addItemToArrayAndRenderList = (item, list, array) => {
+    array.push(item)
+    renderList(list, array)
   }
   
   listTwo.addEventListener("click", (event) => {
     const item = event.target.innerText
-    removeItemFromArrayAndRenderList(item, listTwo)    
+    removeItemFromArrayAndRenderList(item, listTwo, myArray)    
   })
   
-  const removeItemFromArrayAndRenderList = (item, list) => {
+  const removeItemFromArrayAndRenderList = (item, list, array) => {
     // Find the first occurrence of the 
     // item (in this case a string)
     // in the array and return its index location in the array.
-    let index = myArray.indexOf(item)
-    // Remove 1 element from the array at index location called "index".
-    myArray.splice(index, 1)
-    renderList(list, myArray)
+    let index1 = array.indexOf(item)
+    // Finds the first occurrence of where
+    // the predicate is true and returns that index.
+    let index2 = array.findIndex((element) => element == item)
+    // Remove 1 element from the array at index location.
+    array.splice(index2, 1)
+    renderList(list, array)
   }
   
   const renderList = (list, array) => {
