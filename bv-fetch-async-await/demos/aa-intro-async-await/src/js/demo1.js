ee8cbd0cea80620610a2a486880e5d2d8ac80847
robbinl <robbinl@telusplanet.net>
2023-03-29 14:32:06 -0600

    demo1 of fetch

--- bv-fetch-async-await/demos/aa-intro-async-await/src/js/demo1.js
+++ bv-fetch-async-await/demos/aa-intro-async-await/src/js/demo1.js
@@ -8,6 +8,8 @@ export default function Demo1() {
 	const title = document.querySelector('#demo-title')
 	title.innerText = `Demo 1 with local json files`
 
+	const outputDiv = document.querySelector('#output-div')
+
 	const localDataURLGood = './data/bones.json'
 	const localDataURLBadEndPoint = './data/no-file.json'
 	const localDataURLBadData = './data/bad-bones.json'
@@ -25,7 +27,7 @@ export default function Demo1() {
 				"Content-Type": "application/json",
 				'X-Bin-Meta': 'false'
 			},
-		});
+		})
 		console.log(`resolved response with URL: ${URL}`)
 		console.log(res)
 		if (!res.ok) {
@@ -39,15 +41,34 @@ export default function Demo1() {
 		return data
 	}
 
-	const app = async (URL) => {
+	const renderData = (data, ui) => {
+		ui.replaceChildren()
+		let output = ''
+		if(data != null){
+			data.forEach((item) => {
+				output += 
+				`
+					<div class="mb-1">
+						<p>Id: ${item.id}</p>
+						<p>Bone Type: ${item.boneType}</p>
+					</div>
+				`
+			})
+		}
+		ui.insertAdjacentHTML('beforeend', output)
+	}
+
+	const app = async (URL, ui) => {
 		try{
 			console.log(`app try begin with URL ${URL}`)
 			const data = await getData(URL)
+			renderData(data, ui)
 			console.log(`app try end with URL ${URL}`)
 		}
 		catch(error){
 			console.log(`app catch begin with URL ${URL}`)
-			console.error(error)
+			console.error(`The error is: ${error}`)
+			renderData(null, ui)
 			console.log(`app catch end with URL ${URL}`)
 		}
 	}
@@ -60,11 +81,11 @@ export default function Demo1() {
 		count ++
 		console.clear()
 		if(count == 1){
-			app(localDataURLGood)
+			app(localDataURLGood, outputDiv)
 		} else if (count == 2){
-			app(localDataURLBadEndPoint)
+			app(localDataURLBadEndPoint, outputDiv)
 		} else if (count == 3){
-			app(localDataURLBadData)
+			app(localDataURLBadData, outputDiv)
 		} else {
 			console.log(`Press the RESET button to start over`)
 		}
@@ -73,6 +94,7 @@ export default function Demo1() {
 	document.querySelector('#button-reset').addEventListener("click", (e) => {
 		count = 0
 		console.clear()
+		renderData(null, outputDiv)
 		console.log(`Scenarios are reset and ready to run again by pressing the NEXT button`)
 	})
 
