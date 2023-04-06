be9c2f0ae170e5f0f2c7c270323f618b6eff1ed7
robbinl <robbinl@telusplanet.net>
2023-04-06 07:57:02 -0600

    superheros

--- df-node-npm/src/js/index.js
+++ df-node-npm/src/js/index.js
@@ -1,9 +1,6 @@
 // import our bootstrap css
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-// import superheros library
-import 'superheroes'
-
 // import our custom css
 import '../css/main.css'
 
