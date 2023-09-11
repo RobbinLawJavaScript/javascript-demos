598c237fb346fa71d3f9a513a5601dc7f7fe612f
robbinl <robbinl@telusplanet.net>
2023-09-11 11:06:28 -0600

    put word advanced in index.js

--- bc-data-types/src/js/index.js
+++ bc-data-types/src/js/index.js
@@ -4,7 +4,7 @@ import * as myDemo from './data-types.js';
 //import * as myDemo from './arrays.js';
 //import * as myDemo from './object-literals.js';
 
-//Both of these next topics NOT for CPSC1520
+//Both of these next advanced topics NOT for CPSC1520
 //import * as myDemo from './data-type-conversion.js';
 //import * as myDemo from './date-time.js';
 
