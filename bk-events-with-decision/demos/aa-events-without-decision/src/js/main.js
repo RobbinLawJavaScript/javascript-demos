057a110522208ef6d03f67ef61ede17938790fd6
robbinl <robbinl@telusplanet.net>
2023-09-25 09:09:22 -0600

    events update

--- bk-events-with-decision/demos/aa-events-without-decision/src/js/main.js
+++ bk-events-with-decision/demos/aa-events-without-decision/src/js/main.js
@@ -1,26 +1,27 @@
 export default function Demo() {
 
-let counter = 0;
+let counter = 0
 
-const button = document.querySelector('button');
-button.addEventListener('click', ButtonClickHandler);
+const button = document.querySelector('button')
+button.addEventListener('click', ButtonClickHandler)
 function ButtonClickHandler(){
-    counter++;
-    const message = document.querySelector('p#p1');
-    message.innerHTML = `Button was clicked ${counter} times.`;
+    counter++
+    const message = document.querySelector('p#p1')
+    message.innerText = `Button was clicked ${counter} times.`
 }
 
 // Event listener functions will be passed the
-// event object for the event. This object can be
-// used to get information about the event and to 
+// event object for the event by the v8 engine. 
+// This object can be used to get information 
+// about the event and to 
 // call methods/functions on the event as well 
 // e.g. preventDefault()
 
-const naitLink = document.querySelector('a#a1');
-naitLink.addEventListener('click', LinkHandler);
+const naitLink = document.querySelector('a#a1')
+naitLink.addEventListener('click', LinkHandler)
 function LinkHandler(e) {
     //preventDefault stops the normal operation of the href.
-    e.preventDefault();   
+    //e.preventDefault()   
 }
 
 // In the above example, the explicit variable and 
@@ -30,32 +31,18 @@ function LinkHandler(e) {
 
 document.querySelector('a#a2').addEventListener('click', (e) => {
     //preventDefault stops the normal operation of the href.
-    e.preventDefault();
-});
+    //e.preventDefault()
+})
 
 
-const refUl = document.querySelector('ul.links');
-refUl.addEventListener('click', ClickHandler);
+const refUl = document.querySelector('ul.links')
+refUl.addEventListener('click', ClickHandler)
 function ClickHandler(e) {
-    const clickMessage = document.querySelector('p#p2');
-    clickMessage.innerHTML = `you clicked the ${e.target.tagName}`;
-    e.preventDefault();
-}
-
-const refSection = document.querySelector('section.mouse-tricks'); 
-refSection.addEventListener('mouseover', ReportEvent); 
-refSection.addEventListener('mouseout', ReportEvent);
-refSection.addEventListener('mousedown', ReportEvent);
-refSection.addEventListener('mouseup', ReportEvent);
-refSection.addEventListener('click', ReportEvent);
-refSection.addEventListener('dblclick', ReportEvent);
-function ReportEvent(e) { 
-    const bubbles = document.querySelector('#evtBubbles');
-    const tag = document.querySelector('#evtTag');
-    const type = document.querySelector('#evtType');
-    bubbles.innerHTML = e.bubbles;
-    tag.innerHTML = e.target.tagName;
-    type.innerHTML= e.type;
+    e.preventDefault()
+    console.log(e)
+    console.log(e.target)
+    const clickMessage = document.querySelector('p#p3')
+    clickMessage.innerText = `you clicked the ${e.target.tagName}`
 }
 
 }
---
+++ bk-events-with-decision/demos/ee-mouse-tricks-NU/src/js/main.js
@@ -0,0 +1,61 @@
+export default function Demo() {
+
+let counter = 0;
+
+const button = document.querySelector('button');
+button.addEventListener('click', ButtonClickHandler);
+function ButtonClickHandler(){
+    counter++;
+    const message = document.querySelector('p#p1');
+    message.innerHTML = `Button was clicked ${counter} times.`;
+}
+
+// Event listener functions will be passed the
+// event object for the event. This object can be
+// used to get information about the event and to 
+// call methods/functions on the event as well 
+// e.g. preventDefault()
+
+const naitLink = document.querySelector('a#a1');
+naitLink.addEventListener('click', LinkHandler);
+function LinkHandler(e) {
+    //preventDefault stops the normal operation of the href.
+    e.preventDefault();   
+}
+
+// In the above example, the explicit variable and 
+// function are not required as they are not referenced
+// more than once. So, we can rewrite the example all in one statement
+// using an anonymous arrow function.
+
+document.querySelector('a#a2').addEventListener('click', (e) => {
+    //preventDefault stops the normal operation of the href.
+    e.preventDefault();
+});
+
+
+const refUl = document.querySelector('ul.links');
+refUl.addEventListener('click', ClickHandler);
+function ClickHandler(e) {
+    const clickMessage = document.querySelector('p#p2');
+    clickMessage.innerHTML = `you clicked the ${e.target.tagName}`;
+    e.preventDefault();
+}
+
+const refSection = document.querySelector('section.mouse-tricks'); 
+refSection.addEventListener('mouseover', ReportEvent); 
+refSection.addEventListener('mouseout', ReportEvent);
+refSection.addEventListener('mousedown', ReportEvent);
+refSection.addEventListener('mouseup', ReportEvent);
+refSection.addEventListener('click', ReportEvent);
+refSection.addEventListener('dblclick', ReportEvent);
+function ReportEvent(e) { 
+    const bubbles = document.querySelector('#evtBubbles');
+    const tag = document.querySelector('#evtTag');
+    const type = document.querySelector('#evtType');
+    bubbles.innerHTML = e.bubbles;
+    tag.innerHTML = e.target.tagName;
+    type.innerHTML= e.type;
+}
+
+}
