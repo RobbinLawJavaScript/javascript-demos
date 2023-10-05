1e088e917cda536f2872b6d13971324d22a3ace2
robbinl <robbinl@telusplanet.net>
2023-10-05 08:44:55 -0600

    forms exercise

--- bm-forms/exercise/src/js/begin.js
+++ bm-forms/exercise/src/js/begin.js
@@ -3,17 +3,28 @@ export default function Run(){
 	const form = document.querySelector("#form")
 	const list = document.querySelector("#list")
 	
+	form.addEventListener("input", (e) => {
+		if (e.target.id === 'text-id'){
+			console.log(`INPUT textElement Value: ${e.target.value}`)
+			validateText(e.target)
+		}
+	})
+
+	const validateText = (textElement) => {
+		let valid = true
+		//TODO: Add code here to trim the entered text.
+		
+		//TODO: validate the text name element as per specs
+		
+		return valid
+	}
+
 	form.addEventListener("submit", (e) => {
 		e.preventDefault()
 		let textElement = e.target.elements["text-name"]
-		let text = textElement.value
-		//TODO: Add code here to trim the entered text.
-		
-		let isFormValid = true
-		//TODO: validate the text name element as per specs
-		
-		if (isFormValid) {
-			addItemToList(text, list)
+		let isTextValid = validateText(textElement)
+		if (isTextValid) {
+			addItemToList(textElement.value.trim(), list)
 			form.reset()
 		}
 	});
@@ -21,7 +32,9 @@ export default function Run(){
 	function addItemToList(text, list) {
 		let newItem = 
 		`
+		<div class="mb-1 col-6 list-group-item">
 			<p>#${text}</p>
+		</div>
 		`
 		list.insertAdjacentHTML('afterbegin', newItem);
 	}
--- bm-forms/exercise/src/js/end.js
+++ bm-forms/exercise/src/js/end.js
@@ -3,24 +3,33 @@ export default function Run(){
 	const form = document.querySelector("#form")
 	const list = document.querySelector("#list")
 	
+	form.addEventListener("input", (e) => {
+		if (e.target.id === 'text-id'){
+			console.log(`INPUT textElement Value: ${e.target.value}`)
+			validateText(e.target)
+		}
+	})
+
+	const validateText = (textElement) => {
+		let valid = true
+		//TODO: Add code here to trim the entered text.
+		let text = textElement.value.trim()
+		//TODO: validate the text name element as per specs
+		if ((text.length === 0) || (text.length > 10)) {
+			valid = false
+			textElement.classList.add("is-invalid")
+		} else {
+			textElement.classList.remove("is-invalid")	
+		}
+		return valid
+	}
+
 	form.addEventListener("submit", (e) => {
 		e.preventDefault()
 		let textElement = e.target.elements["text-name"]
-		let text = textElement.value
-		//TODO: Add code here to trim the entered text.
-		text = text.trim()
-		let isFormValid = true
-		//TODO: validate the text name element as per specs
-		if ((text === "") || (text.length > 10)) {
-			// text is invalid
-			isFormValid = false
-			textElement.classList.add("is-invalid")
-		} else {
-			// text is valid
-			textElement.classList.remove("is-invalid")
-		}
-		if (isFormValid) {
-			addItemToList(text, list)
+		let isTextValid = validateText(textElement)
+		if (isTextValid) {
+			addItemToList(textElement.value.trim(), list)
 			form.reset()
 		}
 	});
