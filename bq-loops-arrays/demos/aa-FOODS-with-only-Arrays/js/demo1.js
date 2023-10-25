47e64f47c6f1e0e6624fa5cb8ad01231ad13876b
robbinl <robbinl@telusplanet.net>
2023-10-25 13:29:49 -0600

    arrays update

--- bq-loops-arrays/demos/aa-FOODS-with-only-Arrays/js/demo1.js
+++ bq-loops-arrays/demos/aa-FOODS-with-only-Arrays/js/demo1.js
@@ -6,6 +6,8 @@ export function Demo1(data){
   const listTwo = document.querySelector("#list-two")
   const myArray = []
 
+  // Part I
+  // Filtering the original data.
   document.querySelector("#text-box")
   .addEventListener("input", (e) => {
     let filterValue = e.target.value
@@ -30,8 +32,20 @@ export function Demo1(data){
     renderList(filteredData, list)
   }
 
-  listOne.addEventListener("click", (event) => {
-    const item = event.target.innerText
+  const renderList = (array, list) => {
+    list.replaceChildren()
+    array.forEach((element) => {
+      const listItem = `<li class="list-group-item">${element}</li>`
+      list.insertAdjacentHTML('beforeend', listItem)
+    })
+  }
+
+  renderList(data, listOne)
+
+  // Part II
+  // Copying from one array to another.
+  listOne.addEventListener("click", (e) => {
+    const item = e.target.innerText
     addItemToArrayAndRenderList(item, myArray, listTwo)    
   })
 
@@ -40,10 +54,15 @@ export function Demo1(data){
     renderList(array, list)
   }
   
-  listTwo.addEventListener("click", (event) => {
-    const item = event.target.innerText
+  // Part III
+  // Deleting an item from an array.
+  listTwo.addEventListener("click", (e) => {
+    const item = e.target.innerText
     removeItemFromArrayAndRenderList(item, myArray, listTwo)    
   })
+  
+
+  //https://medium.com/front-end-weekly/the-difference-between-indexof-and-findindex-in-javascript-a2035639dce5
   
   const removeItemFromArrayAndRenderList = (item, array, list) => {
     // Find the first occurrence of the 
@@ -54,18 +73,8 @@ export function Demo1(data){
     // the predicate is true and returns that index.
     let index2 = array.findIndex((element) => element == item)
     // Remove 1 element from the array at index location.
-    array.splice(index2, 1)
+    array.splice(index1, 1)
     renderList(array, list)
   }
-  
-  const renderList = (array, list) => {
-    list.replaceChildren()
-    array.forEach((element) => {
-      const listItem = `<li class="list-group-item">${element}</li>`
-      list.insertAdjacentHTML('beforeend', listItem)
-    })
-  }
-
-  renderList(data, listOne)
   
 }
